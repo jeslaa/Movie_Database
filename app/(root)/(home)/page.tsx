842d46aa296f3bcb01900afac185f395aa50513c
jeslaa <jesperlaaksonen@hotmail.com>
2024-05-07 18:03:23 +0200

    Split the homepage into 2 files

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import { MovieCards } from "@/app/components/MovieCards";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   const [banners, setBanners] = useState<Banner[]>([]);
-  const [movies, setMovies] = useState<Movie[]>([]);
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -22,20 +23,7 @@ const Home = () => {
       }
     };
 
-    const fetchMovies = async () => {
-      try {
-        const res = await fetch("/api/movies");
-        if (!res.ok) {
-          throw new Error("Failed to fetch movies");
-        }
-        const movieData = await res.json();
-        setMovies(movieData);
-      } catch (error) {
-        console.error("Error fetching movies:", error);
-      }
-    };
-
-    fetchBanners(), fetchMovies();
+    fetchBanners();
   }, []);
 
   useEffect(() => {
@@ -111,33 +99,7 @@ const Home = () => {
           </Link>
         </div>
       ))}
-      <div className="mt-10 sm:mx-20 relative flex flex-col justify-center items-center">
-        {/* "Movies" title */}
-        <h1 className="text-3xl underline mb-5">Movies</h1>
-
-        {/* Movie banners */}
-        <div className="w-full flex flex-wrap justify-center m-0 p-0">
-          {movies.map((movie, index) => (
-            <div
-              key={index}
-              className="max-w-64 bg-lighterBlack h-56 relative sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-5 sm:mr-5"
-            >
-              <div className="">
-                <img src={movie.image} alt={movie.title} className="" />
-              </div>
-              <div className="flex m-2 flex-col w-48">
-                <p>{movie.score}</p>
-                <p>{movie.title}</p>
-              </div>
-              <Link href={"/"} className="">
-                <div className="absolute bottom-2 right-4 p-4 bg-black rounded-sm">
-                  <FaArrowRight></FaArrowRight>
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
+      <MovieCards />
     </div>
   );
 };
---
+++ app/components/MovieCards.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+
+export const MovieCards = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch("/api/movies");
+        if (!res.ok) {
+          throw new Error("Failed to fetch movies");
+        }
+        const movieData = await res.json();
+        setMovies(movieData);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className="mt-10 sm:mx-20 relative flex flex-col justify-center items-center">
+      {/* "Movies" title */}
+      <h1 className="text-3xl underline mb-5">Movies</h1>
+
+      {/* Movie banners */}
+      <div className="w-full flex flex-wrap justify-center m-0 p-0">
+        {movies.map((movie, index) => (
+          <div
+            key={index}
+            className="max-w-64 bg-lighterBlack h-56 relative sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-5 sm:mr-5"
+          >
+            <div className="">
+              <img src={movie.image} alt={movie.title} className="" />
+            </div>
+            <div className="flex m-2 flex-col w-48">
+              <p>{movie.score}</p>
+              <p>{movie.title}</p>
+            </div>
+            <Link href={"/"} className="">
+              <div className="absolute bottom-2 right-4 p-4 bg-black rounded-sm">
+                <FaArrowRight></FaArrowRight>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
